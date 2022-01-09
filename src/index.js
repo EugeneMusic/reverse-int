78824e157ef5451453712e1a39fe6107a297b6d4
EugeneMusic <muzychenko_eugene@mail.ru>
2022-01-09 19:05:39 +0300

    "feat: add reverse function"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = n.toString().split('');
+    if (result[0] == "-") {
+        result.shift();
+    }
+    result = result.reverse().join('');
+    return +result;
 }
